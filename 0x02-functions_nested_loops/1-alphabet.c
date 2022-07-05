0bf99ef736639058fdc4ac7d51cce6bd720b96ba
OyugiMartin <oyugimartin55@gmail.com>
2022-07-05 13:09:09 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabets in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');	
+}
