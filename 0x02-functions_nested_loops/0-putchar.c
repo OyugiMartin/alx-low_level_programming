bbb0918bdc715f2a68113dee7386b18d11f57de0
OyugiMartin <oyugimartin55@gmail.com>
2022-07-05 12:28:54 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include 'Alx.h'
+
+/**
+ * main - prints _putchar
+ *
+ * Description: Prints _putchar
+ *
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+	char text[10] = " _putchar";
+	int i = 0;
+
+	for( i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/:wq
@@ -0,0 +1,2 @@
+This project is about using headers and nested loops
+
