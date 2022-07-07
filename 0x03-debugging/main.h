f475f6912526979dbc3534364d782ddcd8e5269c
OyugiMartin <oyugimartin55@gmail.com>
2022-07-07 12:45:24 -0700

    Largest Number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,7 @@
+/**
+ * this file contains all functions used
+ */
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -5,4 +9,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
