f5f096f8442632a1e1c21998b2a9a4f77daa8cb9
OyugiMartin <oyugimartin55@gmail.com>
2022-07-11 13:21:05 -0700

    Reset int value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "maiin.h"
+
+/**
+ * reset_to_98 - update value of a pointer to 98
+ *
+ * @n: pointer to an int
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory containd first data on arrays and pointers.
