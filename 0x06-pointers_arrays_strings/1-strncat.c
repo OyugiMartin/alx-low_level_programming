7a06ab9765be40795b1608d22f8a96422d49397c
OyugiMartin <oyugimartin55@gmail.com>
2022-07-14 13:14:04 -0700

    Concentrate n times

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,15 @@
  *
  * Return: pointer to the resulting string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)}
 {
-	char *point;
-	int length;
+	int index = 0, dest_len = 0;
 
-	for (length = 0; dest[length] != '\0'; length++)
-		;
-	point = dest + length;
-	while (*src != '\0' && n--)
-	{
-		*point++ = *src++;
-	}
-	*point = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
