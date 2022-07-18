4de2b839849fa2dfe01ec7acb0c9678a8de191cc
OyugiMartin <oyugimartin55@gmail.com>
2022-07-18 11:35:03 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This file contains Pointers to pointers
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/**
+ * This file contains all functions used
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
